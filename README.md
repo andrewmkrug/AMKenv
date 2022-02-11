2fc963aeff80c4ec0cd1978aed6f4409398d82c8
Andrew M Krug <andrew@lazycoder.io>
2022-02-11 02:20:04 -0500

    feat(importing) reuseable js utils

---
+++ README.md
@@ -0,0 +1,7 @@
+# Shared Script Kit Kenv
+
+The recommended process is to use the url and clone this kenv into your main kenv.
+
+## Setup
+
+Once this kenv is cloned into your run `npm run init` this will copy all of the files into the parent asset directory, until I figure out a way with out needing to hoist the images.
---
+++ scripts/save-tabs.js
@@ -0,0 +1,29 @@
+// Name: Save Tabs
+// Description: Save all tabs to a markdownfile, with support for Obsidian.
+// Author: Andrew M Krug
+// twitter: @andrewmukrug
+
+import "@johnlindquist/kit";
+import browsers from "./browsers.js";
+
+let obsidianDir = await env("NOTES_DIR", "Path to directory for saving tabs");
+log("getting tabs");
+
+let browser = await arg("Which browser:", browsers);
+
+let tabs = await getTabs(browser);
+log("got tabs");
+log("getting filename");
+let date = new Date().toISOString();
+
+let name = await arg("Filename:", [`${date}`]);
+
+let tabsMd = tabs
+  .map((tab) => `* [${tab.title || tab.url}](${tab.url})`)
+  .join("\n");
+
+let notes = await editor(tabsMd);
+log(name);
+log(`${obsidianDir}/${name}`);
+
+await appendFile(`${obsidianDir}/${name}.md`, notes);
