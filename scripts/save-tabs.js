60e5069f612aa0ff5932494196fa526623800177
Andrew M Krug <andrew@lazycoder.io>
2022-02-27 22:00:19 -0500

    February 27, 2022 10:00 PM

--- scripts/save-tabs.js
+++ scripts/save-tabs.js
@@ -3,7 +3,6 @@
 // Author: Andrew M Krug
 // twitter: @andrewmkrug
 // base-img: /assets/LegoGuy.svg
-// img: /Users/andrewmkrug/.kenv/assets/LegoGuy.svg
 // icon: /assets/icon.png
 
 import "@johnlindquist/kit";
