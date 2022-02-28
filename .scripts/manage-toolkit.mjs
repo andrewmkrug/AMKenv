7513deeede4a7172b08ec71ff59233ea6996a960
Andrew M Krug <andrew@lazycoder.io>
2022-02-27 23:38:31 -0500

    February 27, 2022 11:38 PM

--- .scripts/manage-toolkit.mjs
+++ .scripts/manage-toolkit.mjs
@@ -1,2 +1,20 @@
 // .kenv/kenvs/shared/scripts/manage-toolkit.ts
 import "@johnlindquist/kit";
+var envKey = "GIT_MOCK_PATH";
+if (!process.env[envKey]) {
+  let input = await path({
+    startPath: "~/mock-server",
+    hint: "Set a path for the Mocking Server"
+  });
+  await global.cli("set-env-var", envKey, input);
+  global.env[envKey] = process.env[envKey] = input;
+  let repoUrl = `https://github.com/andrewmkrug/toolkit-server.git`;
+  cd(input);
+  await exec(`git clone ${repoUrl} ${input}`);
+  cd(`toolkit-server`);
+  await exec(`npm install`);
+}
+var running = exec(`lsof -nP -iTCP -sTCP:LISTEN | grep 45456`);
+log(running);
+var choice = await arg("What do you want to do?", async () => {
+});
--- scripts/manage-toolkit.ts
+++ scripts/manage-toolkit.ts
@@ -1,4 +1,34 @@
 // Name: Manage Toolkit
+// Author: Andrew M Krug
+// Description: A toolkit for managing your Mocking Server
+// Twitter: @andrewmkrug
 
 import "@johnlindquist/kit"
 
+
+
+const envKey = "GIT_MOCK_PATH";
+
+if (!process.env[envKey]) {
+    let input = await path({
+        startPath: "~/mock-server",
+        hint: "Set a path for the Mocking Server"
+    });
+
+    await global.cli("set-env-var", envKey, input);
+    global.env[envKey] = process.env[envKey] = input;
+
+    let repoUrl = `https://github.com/andrewmkrug/toolkit-server.git`;
+
+    cd(input);
+    await exec(`git clone ${repoUrl} ${input}`);
+    cd(`toolkit-server`);
+    await exec(`npm install`);
+}
+
+let running = exec(`lsof -nP -iTCP -sTCP:LISTEN | grep 45456`)
+dev(running);
+
+let choice = await arg("What do you want to do?", async () => {
+
+})
