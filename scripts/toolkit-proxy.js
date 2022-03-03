a3b18456e2c582a967746e391d79f8bec823c954
Andrew M Krug <andrew@lazycoder.io>
2022-03-03 08:50:36 -0500

    March 3, 2022 8:50 AM

--- scripts/toolkit-proxy.js
+++ scripts/toolkit-proxy.js
@@ -1,4 +1,29 @@
 // Name: Toolkit Proxy
+// Author: Andrew M Krug
+// Description: A toolkit for managing your Mocking Server
+// Twitter: @andrewmkrug
 
-import "@johnlindquist/kit"
+import "@johnlindquist/kit";
 
+const envKey = "GIT_MOCK_PATH";
+
+if (!process.env[envKey]) {
+  let input = await path({
+    startPath: "~/mock-server",
+    hint: "Set a path for the Mocking Server"
+  });
+
+  await global.cli("set-env-var", envKey, input);
+  global.env[envKey] = process.env[envKey] = input;
+
+  let repoUrl = `https://github.com/andrewmkrug/toolkit-server.git`;
+
+  cd(input);
+  await exec(`git clone ${repoUrl} ${input}`);
+  cd(`toolkit-server`);
+  await exec(`npm install`);
+}
+
+terminal(`cd ${env(envKey)} && npm run start`);
+
+browse(`https://toolkit.krugweb.services`);
